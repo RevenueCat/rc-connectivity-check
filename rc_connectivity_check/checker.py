37e2e0a95dbfe079252d7327b3a9b86c9fb6fc7d
Guillermo Perez <bisho@revenuecat.com>
2024-06-13 22:57:23 +0200

    Improve checks and guidance
    * Use urllib3 directly to be able to use HTTP and HTTPS checks,
    overriding the IP used for connection while still doing SNI
    and SSL certificate validation.
    * Collect failed IPs and suggest further debugging commands

--- rc_connectivity_check/checker.py
+++ rc_connectivity_check/checker.py
@@ -1,11 +1,13 @@
+import argparse
 from collections import defaultdict
 import socket
-from typing import DefaultDict, Dict, List, NamedTuple
+from typing import DefaultDict, Dict, List, NamedTuple, Tuple
 import requests
 import time
 from statistics import mean
 from concurrent.futures import Future, ThreadPoolExecutor, as_completed
 from colorama import Fore, Style, init
+import urllib3
 
 
 # Initialize colorama
@@ -32,29 +34,67 @@ class TestResult(NamedTuple):
     avg_latency: float
 
 
+def get_request_on_custom_ip(
+    ip: str,
+    domain: str,
+    location: str,
+    https: bool,
+    timeout: int,
+) -> Tuple[int, bytes]:
+    if https:
+        pool = urllib3.HTTPSConnectionPool(
+            ip,
+            server_hostname=domain,
+            timeout=timeout,
+            retries=False,
+        )
+    else:
+        pool = urllib3.HTTPConnectionPool(
+            ip,
+            timeout=timeout,
+            retries=False,
+        )
+    response = pool.request(
+        "GET",
+        location,
+        headers={"Host": domain},
+        redirect=False,
+    )
+    return response.status, response.data
+
+
 # Function to issue HTTP requests and measure latencies
-def measure_latency(ip: str, hostname: str) -> TestResult:
-    url = f"http://{ip}/favicon-32x32.png"
-    headers = {"Host": hostname}
+def measure_latency(
+    ip: str, hostname: str, https: bool = False, timeout: int = 5
+) -> TestResult:
     latencies: List[float] = []
     failures = 0
 
     for _ in range(10):
         start_time = time.time()
         try:
-            response = requests.get(url, headers=headers, timeout=5)
-            if hostname == "api.revenuecat.com" and response.status_code == 404:
-                pass
+            status_code, _ = get_request_on_custom_ip(
+                ip, hostname, "/favicon-32x32.png", https, timeout
+            )
+            if status_code < 400:
+                ok = True
+            elif status_code == 404 and hostname == "api.revenuecat.com":
+                ok = True
             else:
-                response.raise_for_status()
-        except (requests.RequestException, requests.Timeout) as e:
+                ok = False
+
+            if not ok:
+                raise Exception(
+                    f"Failed to fetch from {hostname} at {ip}:{443 if https else 80}: {status_code}"
+                )
+        except Exception as e:
             print(f"{error('ERROR')}: Failed to connect to {hostname} at {ip}: {e}")
             failures += 1
         latency = time.time() - start_time
         latencies.append(latency)
 
     return TestResult(
-        hostname=hostname,
+        hostname=f"{'https' if https else 'http'}://{hostname}",
         ip=ip,
         failures=failures,
         min_latency=min(latencies, default=0),
@@ -100,6 +140,15 @@ def find_client_ip() -> str:
 
 # Main logic
 def main():
+    parser = argparse.ArgumentParser(description="RevenueCat Connectivity Check tool")
+    parser.add_argument(
+        "--timeout",
+        type=int,
+        default=5,
+        help="Request timeout, in seconds (default: 5)",
+    )
+    args = parser.parse_args()
+
     print("Finding client Public IP...")
     ip = find_client_ip()
     print(f"Client Public IP: {color(ip, Fore.YELLOW)}")
@@ -123,7 +172,12 @@ def main():
         futures: List[Future[TestResult]] = []
         for hostname, ips in hostname_ips.items():
             for ip in ips:
-                futures.append(executor.submit(measure_latency, ip, hostname))
+                for https in [True, False]:
+                    futures.append(
+                        executor.submit(
+                            measure_latency, ip, hostname, https, args.timeout
+                        )
+                    )
 
         results: DefaultDict[str, List[TestResult]] = defaultdict(list)
         for future in as_completed(futures):
@@ -135,6 +189,43 @@ def main():
 
         for hostname, res in results.items():
             print_results(hostname, res)
+
+        failed_ips = set()
+        for res in results:
+            for result in results[res]:
+                if result.failures > 0:
+                    failed_ips.add(result.ip)
+
+        if failed_ips:
+            print()
+            print(error("There were some connectivity failures!"))
+            print("Please help us investigate by following the next steps:")
+            print()
+            print("1: Check network routes:")
+            print("  - Install mtr:")
+            print("    - macOS: brew install mtr")
+            print("    - Ubuntu: sudo apt-get install mtr")
+            print("  - Run mtr using the following command:")
+            print(
+                "    sudo mtr --report -n --tcp --port=443 --gracetime=5 --timeout=5 "
+                + " ".join(failed_ips)
+            )
+            print()
+            tcpdump_cmd = "tcpdump -i <INTERFACE> -w rc_connectivity_check.pcap -s 0 tcp and port 80"
+            print("2: Capture network traffic while running this script:")
+            print("  - In one terminal run:")
+            print("    - On macOS, over WIFI:")
+            print("      " + tcpdump_cmd.replace("<INTERFACE>", "en0"))
+            print("    - On macOS, over ethernet:")
+            print("      " + tcpdump_cmd.replace("<INTERFACE>", "en1"))
+            print("    - On linux, find the interface name with ifconfig and use:")
+            print("      " + tcpdump_cmd)
+            print("  - In another terminal, run this script again:")
+            print("    rc-connectivity-check")
+            print("  - Once completed, stop the tcpdump process with `Ctrl+C`")
+            print(
+                "  - Send the resulting rc_connectivity_check.pcap file to RevenueCat support"
+            )
 
 
 if __name__ == "__main__":
